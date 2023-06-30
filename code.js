01763eb3ea8e0cefb2e91459d29f0c0058503351
Marinos <marinos1366@gmail.com>
2023-06-30 17:48:18 +0300

    secret_header

--- code.js
+++ code.js
@@ -1,4 +1,5 @@
 var header = document.querySelector("header");
+var scroll_header = document.querySelector(".scroll_header");
 
 window.onscroll = function () {
   scrollFunction();
@@ -7,7 +8,9 @@ window.onscroll = function () {
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
     header.classList.add("header_transform");
+    scroll_header.classList.add("scroll_header_active");
   } else {
     header.classList.remove("header_transform");
+    scroll_header.classList.remove("scroll_header_active");
   }
 }

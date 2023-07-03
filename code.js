b1412dbda94062a5994b2bee17f102f84f982ddf
Marinos <marinos1366@gmail.com>
2023-07-03 12:25:20 +0300

    headerfooter

--- code.js
+++ code.js
@@ -1,16 +1,11 @@
-var header = document.querySelector("header");
-var scroll_header = document.querySelector(".scroll_header");
-
 window.onscroll = function () {
   scrollFunction();
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    header.classList.add("header_transform");
-    scroll_header.classList.add("scroll_header_active");
+  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+    document.getElementById("header").classList.add("header");
   } else {
-    header.classList.remove("header_transform");
-    scroll_header.classList.remove("scroll_header_active");
+    document.getElementById("header").classList.remove("header");
   }
 }

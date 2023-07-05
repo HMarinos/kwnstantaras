5d4c8adc770d38c4c9f3e8a35b56010f410d8d98
Marinos <marinos1366@gmail.com>
2023-07-05 12:38:16 +0300

    dropdown-pre-blur

--- code.js
+++ code.js
@@ -5,7 +5,9 @@ window.onscroll = function () {
 function scrollFunction() {
   if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
     document.getElementById("header").classList.add("header");
+    document.getElementById("dropdown").classList.add("dropdown_after");
   } else {
     document.getElementById("header").classList.remove("header");
+    document.getElementById("dropdown").classList.remove("dropdown_after");
   }
 }
